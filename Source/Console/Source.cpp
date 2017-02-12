8a1c54a40ddd47b64f6ee95f338da9c03649b8c7
Great V <GreatV@email.ua>
2017-02-12 13:47:46 +0200

    [Update] Console using stringify

--- Source/Console/Source.cpp
+++ Source/Console/Source.cpp
@@ -10,18 +10,19 @@ using namespace JSON;
 
 void main()
 {
-	auto t = JSON::Parse("{\"a\":null,\"b\":1}");
+	JSON::Object obj;
+	{
+		obj["a"] = true;
+		obj["b"] = 5;
+		obj["c"] = "ololo";
+		obj["top kek"] = {{5, true}, "ololo"};
+	}
+	
+	std::cout << obj.Stringify() << std::endl;
 
-	cout << (string)t << endl;
+	auto parsed = JSON::Parse(obj.Stringify());
+	cout << parsed.Stringify() << endl;
 
-	// JSON::Object obj; // = {{5, true}, "ololo"};
-	// {
-	// 	// obj["a"] = true;
-	// 	// obj["b"] = 5;
-	// 	// obj["c"] = "ololo";
-	// }
-	// 
-	// std::cout << (std::string)obj << std::endl;
 
 	system("pause");
 }
